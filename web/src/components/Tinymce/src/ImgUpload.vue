252c2bac1537252149a6959d88df2bdfce29aca2
van <hello@van.cn>
2023-10-31 22:27:45 +1100

    first commit

---
+++ web/src/components/Tinymce/src/ImgUpload.vue
@@ -0,0 +1,103 @@
+<template>
+  <div :class="[prefixCls, { fullscreen }]">
+    <Upload
+      name="file"
+      multiple
+      @change="handleChange"
+      :action="uploadUrl"
+      :headers="header"
+      :showUploadList="false"
+      accept=".jpg,.jpeg,.gif,.png,.webp"
+    >
+      <a-button type="primary" v-bind="{ ...getButtonProps }">
+        {{ t('component.upload.imgUpload') }}
+      </a-button>
+    </Upload>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, computed, reactive } from 'vue';
+
+  import { Upload } from 'ant-design-vue';
+  import { useDesign } from '/@/hooks/web/useDesign';
+  import { useGlobSetting } from '/@/hooks/setting';
+  import { useI18n } from '/@/hooks/web/useI18n';
+  import { getToken } from '/@/utils/auth';
+
+  export default defineComponent({
+    name: 'TinymceImageUpload',
+    components: { Upload },
+    props: {
+      fullscreen: {
+        type: Boolean,
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    emits: ['uploading', 'done', 'error'],
+    setup(props, { emit }) {
+      let uploading = false;
+
+      const { uploadUrl } = useGlobSetting();
+      const { t } = useI18n();
+      const { prefixCls } = useDesign('tinymce-img-upload');
+
+      const getButtonProps = computed(() => {
+        const { disabled } = props;
+        return {
+          disabled,
+        };
+      });
+
+      const header = reactive({
+        Authorization: getToken(),
+      });
+
+      function handleChange(info: Recordable) {
+        const file = info.file;
+        const status = file?.status;
+        const id = file?.response?.result.id;
+        const name = file?.name;
+
+        if (status === 'uploading') {
+          if (!uploading) {
+            emit('uploading', name);
+            uploading = true;
+          }
+        } else if (status === 'done') {
+          emit('done', name, id);
+          uploading = false;
+        } else if (status === 'error') {
+          emit('error');
+          uploading = false;
+        }
+      }
+
+      return {
+        prefixCls,
+        handleChange,
+        uploadUrl,
+        header,
+        t,
+        getButtonProps,
+      };
+    },
+  });
+</script>
+<style lang="less" scoped>
+  @prefix-cls: ~'@{namespace}-tinymce-img-upload';
+
+  .@{prefix-cls} {
+    position: absolute;
+    top: 4px;
+    right: 10px;
+    z-index: 20;
+
+    &.fullscreen {
+      position: fixed;
+      z-index: 10000;
+    }
+  }
+</style>
